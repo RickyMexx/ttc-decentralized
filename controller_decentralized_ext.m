d75627ea7422f634678cadd7588ab53dda791917
Emanuele Giacomini <emanuele.giacomo@gmail.com>
2020-09-17 10:55:42 +0200

    Added images, controller_decentralized and controller_decentralized_ext

--- controller_decentralized_ext.m
+++ controller_decentralized_ext.m
@@ -1,5 +1,6 @@
-function u = controller_decentralized(q, dq, ddq, a, D, N, kp, kd, err, err_prec)
-%CONTROLLER_DECENTRALIZED Decentralized controller
+function u = controller_decentralized_ext(q, dq, ddq, a, D, N, kp, kd, err, err_prec)
+%CONTROLLER_DECENTRALIZED Decentralized controller extended with non-linear
+%estimate
 %[q,dq,ddq] : Robot State variables 
 %       a   : Robot Dynamic coefficients
 %       D   : Viscous Friction matrix
@@ -14,12 +15,16 @@ function u = controller_decentralized(q, dq, ddq, a, D, N, kp, kd, err, err_prec
 % [305, (8.2)] N * q = qm
 % [306, (8.3)] inv(N) * t = tm
 
-[Mbar, ~] = eval_2r_M_decomp(a, q);
+[Mbar, dM] = eval_2r_M_decomp(a, q);
+C = eval_2r_C(a, q, dq);
+G = eval_2r_G(a, q);
 Ni = inv(N);
+
 % Compute ddq_targ
 ddqm_targ = (kp * err + kd * (err - err_prec));
+% Configuration dependant contribution
+d = Ni * dM * Ni * ddqm_targ + Ni * C * Ni * (N * dq) + Ni * G;
 % Convert to motor state variables
-u_m = Ni * Mbar * Ni * ddqm_targ + (Ni * D * Ni) * (N * dq);
+u_m = Ni * Mbar * Ni * ddqm_targ + (Ni * D * Ni) * (N * dq) + d;
 u = N * u_m;
 end
-
